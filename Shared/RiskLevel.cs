2dd45df8fb6e1db6c3e09370672aee779608a2e1
Cedric Clancy <cedlinx@yahoo.com>
2025-05-30 00:10:29 +0100

    Initial commit: SelfAdaptiveAgent solution with Functions, Web, Shared

---
+++ Shared/RiskLevel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public enum RiskLevel { Low = 0, Medium = 1, High = 2 }
+}
+
