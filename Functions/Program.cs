6bfc03abc3b7690ff92632047ec66e62afb87482
Cedric Clancy <cedlinx@yahoo.com>
2025-05-30 05:08:24 +0100

    just added a comment4

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -6,7 +6,7 @@ var builder = FunctionsApplication.CreateBuilder(args);
 builder.ConfigureFunctionsWebApplication();
 
 // Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
-// builder.Services
+// builder.Services...
 //     .AddApplicationInsightsTelemetryWorkerService()
 //     .ConfigureFunctionsApplicationInsights();
 
